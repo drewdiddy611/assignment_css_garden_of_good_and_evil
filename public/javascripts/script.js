4ccc1dc2a029dd330992cb3cf667d40252ac6ffd
Ian Halverson <halversoncode@gmail.com>
2017-07-24 17:02:07 -0400

    working yay

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,28 @@
 // With JQuery
 $(document).ready(function() {
-	$('#ex1').slider();
-	populateFormFields();
+  $("#insanity").slider();
+  populateFormFields();
 });
 
-function populateFormFields() {}
+var q = {
+  faction: str => {
+    $(`#faction-${str}`).prop("checked", true);
+  },
+  food: val => $("#food").val(val),
+  color: val => $("#color").val(val),
+  insanity: val => $("#insanity").slider("setValue", val)
+};
+
+function populateFormFields() {
+  getParsedCookies().forEach(el => {
+    if (el[0].length > 0 && $.isFunction(q[el[0]])) {
+      q[el[0]](el[1]);
+    }
+  });
+}
+
+function getParsedCookies() {
+  return document.cookie
+    .split(";")
+    .map(el => decodeURIComponent(el.trim()).split("="));
+}
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const cookieMap = ['food', 'color', 'faction', 'insanity'];
+const cookieMap = ["food", "color", "faction", "insanity"];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', {
-		title: 'The Kingdom of Good & Evil'
-	});
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "The Kingdom of Good & Evil"
+  });
 });
 
-router.post('/', function(req, res, next) {
-	for (k in req.body) {
-		res.cookie(k, req.body[k]);
-	}
-	res.redirect('/');
+router.post("/", function(req, res, next) {
+  for (k in req.body) {
+    res.cookie(k, req.body[k]);
+  }
+  res.redirect("/");
 });
 
 module.exports = router;
