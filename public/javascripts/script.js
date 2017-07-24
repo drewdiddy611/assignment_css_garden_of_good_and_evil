d4afc703ef77a0f01199605c7d8235b4ed53e4f4
Andrew Senner <me@andrewsenner.com>
2017-07-24 18:56:09 -0400

    done?

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,34 @@
 // With JQuery
+var insaneKittens = false;
 $(document).ready(function() {
-	$('#insanity').slider();
-
 	// Get parsed cookies.
 	let parsedCookies = getParsedCookies();
 	if (parsedCookies.length === 4) {
+		$($('#insanity').slider()).on('change', e => {
+			confirmInsanity(e, parsedCookies[2][1]);
+		});
+
 		populateFormFields(parsedCookies).then(startInsanity);
 	}
 });
+
+function confirmInsanity(e, color) {
+	let $slider = $(e.target);
+	let value = $slider.slider('getValue');
+	if (value === 20 && !insaneKittens) {
+		if (
+			window.confirm('Are you sure you want to experience insanity level 20?')
+		) {
+			$('#insanity').off('change', confirmInsanity);
+			insaneKitties();
+			insaneKittens = true;
+			$slider.slider('setValue', 1);
+		} else {
+			$slider.slider('setValue', 19);
+		}
+	}
+	titleInsane(value, color);
+}
 
 var insanityIntervalID = 0;
 
@@ -43,7 +64,13 @@ function startInsanity() {
 
 	$('body').addClass(parsedCookies[0][1]); // Faction.
 
+	console.log(`.faction-${parsedCookies[0][1]}`);
+	// Show resume.
+	$(`.faction-${parsedCookies[0][1]}`).removeClass('hidden');
+
 	titleInsane(parsedCookies[3][1], parsedCookies[2][1]);
+
+	//insaneKitties();
 }
 
 function titleInsane(sanityLevel, color) {
@@ -55,6 +82,7 @@ function titleInsane(sanityLevel, color) {
 	// Set the sanity level.
 	sanityLevel = 1000 / sanityLevel;
 	let $title = $('h1');
+	$title.css({ color: '#333' });
 
 	var diffColor = false,
 		oldColor = $title.css('color');
@@ -63,4 +91,51 @@ function titleInsane(sanityLevel, color) {
 		$title.css({ color: diffColor ? color : oldColor });
 		diffColor = !diffColor;
 	}, sanityLevel);
+}
+
+jQuery.fn.rotate = function(degrees) {
+	$(this).css({ transform: 'rotate(' + degrees + 'deg)' });
+	return $(this);
+};
+
+function insaneKitties() {
+	let $container = $('<div/>', {
+		class: 'kitty-container'
+	});
+
+	// Container.
+	$('body').append($container);
+
+	var wheight = $(window).height(); // returns height of browser viewport
+	var wwidth = $(window).width(); // returns width of browser viewport
+
+	let $kitty = $('<img/>', {
+		class: 'kitty-image',
+		src: ''
+	});
+	var kittyInterval = setInterval(function() {
+		// Determine which kitty.
+		let i = Math.floor(Math.random() * 5);
+
+		// Determine position.
+		let x = Math.floor(Math.random() * wwidth);
+		let y = Math.floor(Math.random() * wheight);
+
+		// Determine rotation.
+		let r = Math.floor(Math.random() * 359);
+
+		$kitty
+			.clone()
+			.attr('src', 'images/kitten' + i + '.png')
+			.rotate(r)
+			.css({
+				left: x,
+				top: y
+			})
+			.appendTo($container)
+			.addClass('kitty-rotate');
+	}, 10);
+	setTimeout(() => {
+		clearInterval(kittyInterval);
+	}, 100000);
 }
